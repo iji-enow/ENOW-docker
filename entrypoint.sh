af0f6e2c721ce86e2e95bd38c3a71ae3cdc9fb0b
writtic <writtic@gmail.com>
2016-09-19 15:52:23 +0900

    Update docker-compose.yml for submitting topologies

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CMD="exec bin/storm jar ./topology.jar com.enow.storm.TestTopologyStaticHosts Enow "
+CMD="exec bin/storm jar ./topology.jar com.enow.storm.main.main Action Trigger"
 
 echo "$CMD"
 eval "$CMD"
